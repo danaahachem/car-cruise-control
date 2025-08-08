58c3c75bf86db8bbe2eb06f5918bdd6cfd780bfe
Dana Hachem <danaahachem@gmail.com>
2025-08-08 22:34:27 +0300

    Delete CarCruiseController directory

--- CarCruiseController/car_dynamics.m
+++
@@ -1,9 +0,0 @@
-function dvdt = car_dynamics(t, v, m, b, g, F_engine)
-    if t < 10
-    theta = 0.05;   % uphill
-    else
-        theta = -0.05;  % downhill
-    end
-    dvdt = (1/m) * (F_engine - b*v - m*g*sin(theta));
-end
-
